9d013362afe9052559ecfed24bade1ece6b883d8
Sufike <l.christ54260@gmail.com>
2018-12-16 14:03:35 +0100

    finish del kit

--- pumpmykitbukkit/src/main/java/fr/pumpmykitbukkit/KitDelCommand.java
+++ pumpmykitbukkit/src/main/java/fr/pumpmykitbukkit/KitDelCommand.java
@@ -5,11 +5,20 @@ import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
+import fr.pumpmykitcore.command.CommandHandler;
+import fr.pumpmykitcore.utils.Utils;
+
 public class KitDelCommand implements ISubCommand {
 
 	@Override
 	public boolean onSubCommand(Player sender, Command cmd, List<String> args) {
-		// TODO Auto-generated method stub
+		
+		if(sender.hasPermission("kit.del")) {
+			
+			Utils.debug(args.get(1));
+			Utils.debug(args);
+			CommandHandler.deleteKit(args.get(1));
+		}
 		return false;
 	}
 
