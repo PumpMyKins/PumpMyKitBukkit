29f9d8de7e325071d71f9d9747958863e57bba1e
Sufike <l.christ54260@gmail.com>
2019-01-04 20:17:51 +0100

    handle command for devlopment

--- pumpmykitbukkit/src/main/java/fr/pumpmykitbukkit/Command/KitBuyCommand.java
+++ pumpmykitbukkit/src/main/java/fr/pumpmykitbukkit/Command/KitBuyCommand.java
@@ -2,9 +2,11 @@ package fr.pumpmykitbukkit.Command;
 
 import java.sql.Timestamp;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import fr.pumpmykitcore.utils.KitsUtils;
 import fr.pumpmykitcore.utils.Utils;
@@ -16,12 +18,14 @@ public class KitBuyCommand implements CommandExecutor {
 
 		//Schéma Commmande /buykit idPurchase buyeruuid kitname
 		//SChéma KitsUtils : String buyerUuid, String kitname, Timestamp date, String idPurchase
-		
+		/*
 		Timestamp timekit = new Timestamp(System.currentTimeMillis());
 		Utils.debug(args);
 		KitsUtils.kitBuyLog(args[1], args[2], timekit , args[0]);
+		*/
+		Player sufike = Bukkit.getPlayer(sender.getName());
+		Timestamp date = new Timestamp(System.currentTimeMillis());
+		KitsUtils.kitBuyLog(sufike.getUniqueId().toString(), args[0], date, args[1]);
 		return false;
-	}
-
-	
+	}	
 }
